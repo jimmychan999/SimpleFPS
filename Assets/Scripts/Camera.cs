c0343f76de043552987d31dd6addb453149130ad
Jimmy Chan <jimhaotra1234@hotmail.com>
2022-08-11 02:06:19 +0200

    Init project

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public PlayerMovement player;
+    public float rotateSpeed = 400f;
+    float rotationX;
+    float rotationY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * rotateSpeed;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * rotateSpeed;
+        Debug.Log(mouseX + " " + mouseY);
+
+        rotationX -= mouseY;
+        rotationY += mouseX;
+
+        Quaternion rotation = Quaternion.Euler(0f, rotationY, 0f);
+        Quaternion rotationVert = Quaternion.Euler(rotationX, 0f, 0f);
+
+
+        player.transform.rotation = rotation;
+        transform.localRotation = rotationVert;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float playerSpeed = 5f;
+    private Rigidbody rb;
+    public float jumpForce = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Player movement
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * playerSpeed);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * -playerSpeed);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * playerSpeed);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * -playerSpeed);
+        }
+
+        // Player jump
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce);
+        }
+    }
+}
